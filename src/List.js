f994c22987f8bdd521219b872941ed641151987f
Reshilobeth <92940855+Reshilobeth@users.noreply.github.com>
2022-07-07 18:47:07 +0530

    first commit

---
+++ src/List.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const List = ({people}) => {
+  return (
+    <>
+    {people.map((person) =>{
+        const {id, name, age, image} = person
+        return <div key={id} className = 'person'>
+            <img src={image} alt ='Profilephoto' />
+            <div>
+                <h3>{name}</h3>
+                <p>{age} Years</p>
+            </div>
+        </div>
+    })}
+    </>
+  )
+}
+
+export default List
